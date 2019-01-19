013f4bc8160f76cff3bcba390fa8c7befccf2783
jpoh97 <jpoh97@gmail.com>
2019-01-18 22:47:09 -0500

    Implemented quicksort method

--- QuickSort/src/co/com/jpoh97/quicksort/Main.java
+++ QuickSort/src/co/com/jpoh97/quicksort/Main.java
@@ -1,8 +1,20 @@
 package co.com.jpoh97.quicksort;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println(Arrays.toString(intArray));
+    }
+
+    private static void quickSort(int[] input, int start, int end) {
+        if (end - start < 2) return;
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex + 1, end);
     }
 }
